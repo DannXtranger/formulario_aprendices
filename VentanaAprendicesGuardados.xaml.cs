247a1d882b91fbdb21a434aa9d90e23b819e9f16
Dann <robindjimenezf@gmail.com>
2024-03-17 23:12:09 -0500

    Creé el repositorio

---
+++ VentanaAprendicesGuardados.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace formulario_aprendices
+{
+    /// <summary>
+    /// Interaction logic for VentanaAprendicesGuardados.xaml
+    /// </summary>
+    public partial class VentanaAprendicesGuardados : Window
+    {
+        string[] doc = null;
+        public VentanaAprendicesGuardados()
+        {
+            InitializeComponent();
+            CargarDataGrid();
+        }
+
+        private void CargarDataGrid()
+        {
+            using(PracticaDataGridEntities bd  = new PracticaDataGridEntities()) 
+            {
+               var consulta = from aprendiz in bd.aprendices
+                               join ciudad in bd.ciudades on aprendiz.idCiudad equals ciudad.id
+                               join tipoDocumento in bd.tiposDocumentos on aprendiz.idTipoDocumento equals tipoDocumento.id
+                               join genero in bd.generos on aprendiz.idGenero equals genero.id
+                               select new
+                               {
+                                 tipoDeDocumento = tipoDocumento.abrev_tipo_doc,
+                                 doc =  aprendiz.documento,
+                                 nombre =  aprendiz.nombre,
+                                 edad = SqlFunctions.DateDiff("year", aprendiz.fechaNacimiento, DateTime.Now),
+                                 genero = genero.abrev_genero,
+                                 ciudad =  ciudad.nombre_ciudad
+
+                               };
+                var datosAprendiz = consulta.ToList();  
+
+                dataGridAprendices.ItemsSource = datosAprendiz;
+
+
+            }
+        }
+
+        private void BtnAtras_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow ventanaPrincipal = new MainWindow();
+            ventanaPrincipal.Show();
+            Close();
+        }
+
+        private void dataGridAprendices_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && e.ClickCount == 2 && dataGridAprendices.SelectedItem != null)
+            {
+
+                DataGridRow row = (DataGridRow)dataGridAprendices.ItemContainerGenerator.ContainerFromItem(dataGridAprendices.SelectedItem);
+
+                if (row != null)
+                {
+                    int columnIndex = 1;
+                    DataGridCell cell = dataGridAprendices.Columns[columnIndex].GetCellContent(row).Parent as DataGridCell;
+                    
+
+                       doc = cell.ToString().Split(' ');
+                       // MessageBox.Show(doc[1].ToString());
+
+                    
+                    cmOpciones.IsOpen = true;
+                    
+
+
+                }
+            }
+            
+        }
+
+ 
+
+        private void MenuItem_Click_Eliminar(object sender, RoutedEventArgs e)
+        {
+
+            using(var db = new PracticaDataGridEntities())
+            {
+                var documento = doc[1].ToString().Trim();
+                var aprendizAEliminar = db.aprendices.Where(
+                    a => a.documento.ToString().Trim() == documento).ToList();
+
+                db.aprendices.RemoveRange(aprendizAEliminar);
+                db.SaveChanges();
+                MessageBox.Show("Registro Borrado Con Éxito");
+            }
+            CargarDataGrid();
+        }
+
+        private void MenuItem_Click_Modificar(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
---
+++ ventanaGuardar.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace formulario_aprendices
+{
+    /// <summary>
+    /// Interaction logic for ventanaGuardar.xaml
+    /// </summary>
+    public partial class ventanaGuardar : Window
+    {
+        public ventanaGuardar()
+        {
+            InitializeComponent();
+            LlenarComboTiposDocumentos();
+            LlenarComboCiudades();
+        }
+
+        private void LlenarComboCiudades()
+        {
+            using(var db = new PracticaDataGridEntities())
+            {
+                var ciudades = from ciudad in db.ciudades
+                               select ciudad;
+
+                cmbCiudad.SelectedValuePath = "id";
+                cmbCiudad.DisplayMemberPath = "nombre_ciudad";
+                cmbCiudad.ItemsSource = ciudades.ToList();
+            }
+        }
+
+        private void LlenarComboTiposDocumentos()
+        {
+            using (var db = new PracticaDataGridEntities())
+            {
+                var tipoDeDocumentos = from tipoDoc in db.tiposDocumentos
+                                      select tipoDoc;
+                cmbTipoDocumento.SelectedValuePath = "id";
+                cmbTipoDocumento.DisplayMemberPath = "nombre_tipo_doc";
+                cmbTipoDocumento.ItemsSource = tipoDeDocumentos.ToList();
+            }
+        }
+     
+
+        private void BtnAtras_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow ventanaPrincipal = new MainWindow();
+            ventanaPrincipal.Show();
+            Close();
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            aprendices aprendiz = new aprendices();
+
+            using(var db = new PracticaDataGridEntities())
+            {
+                try
+                {
+                    aprendiz.nombre = txtNombre.Text.Trim() + " " + txtApellido.Text.Trim();
+                    aprendiz.documento = int.Parse(txtDocumento.Text);
+                    aprendiz.idTipoDocumento = int.Parse(cmbTipoDocumento.SelectedValue.ToString());
+                    aprendiz.idCiudad = int.Parse(cmbCiudad.SelectedValue.ToString());
+                    aprendiz.idGenero = EscogerGenero();
+                    aprendiz.fechaNacimiento = dateFechaNac.SelectedDate;
+
+                    db.aprendices.Add(aprendiz);
+                    db.SaveChanges();
+
+                }catch(Exception ex)
+                {
+                    MessageBox.Show("Error");
+                }
+
+                MessageBox.Show("Aprendiz registrado con éxito");
+
+            }
+        }
+
+        private int EscogerGenero()
+        {
+            int idGenero = 1;
+            if(rdFemenino.IsChecked == true)
+            {
+                idGenero = 2;
+            }
+
+            return idGenero;
+        }
+    }
+}
